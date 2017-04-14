f03b5609aa5e6bd89963b0da34dcece99a4bfaf2
yini <nini>
2017-04-14 14:03:51 +0800

    first

---
+++ src/test/java/test/functional/product/AServiceFunctionalTest.java
@@ -0,0 +1,15 @@
+package test.functional.product;
+
+/**
+ * Created by cypc on 4/14/17.
+ */
+public class AserveiceTest {
+    private static AserveiceTest ourInstance = new AserveiceTest();
+
+    public static AserveiceTest getInstance() {
+        return ourInstance;
+    }
+
+    private AserveiceTest() {
+    }
+}
